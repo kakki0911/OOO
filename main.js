984a4ee95a6e8e5777d86e98e092a2ee140fd4f7
kakki0911 <145298359+kakki0911@users.noreply.github.com>
2023-11-07 06:15:36 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+'use strict'
+
+{
+  const btn = document.getElementById('btn');
+  const result = document.getElementById('result');
+
+  btn.addEventListener('click', () => {
+    const results = ['大吉', '中吉', '凶', '小吉', '大吉', '中吉', '凶', '小吉', '💩', '👻', 'もり吉'];
+    const n = Math.floor(Math.random() * results.length);
+
+    result.textContent = results[n];
+
+    // switch (n) {
+    //   case 0:
+    //     result.textContent = '大吉';
+    //     break;
+    //   case 1:
+    //     result.textContent = '中吉';
+    //     break;
+    //   case 2:
+    //     result.textContent = '凶';
+    //     break;
+    // }
+
+  });
+}
